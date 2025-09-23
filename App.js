e8f4e989045c5fe6bfa50c2130ac9d8e4db30fb6
AniketXD1 <tsmaniket@gmail.com>
2025-09-23 14:38:23 +0530

    episode.2

--- App.js
+++ App.js
@@ -1,28 +1,3 @@
-// how to create h1 tag in react
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "hello world from javascript!"
-// );
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-
-// Nested Element
-
-/* <div id="parent">
-  <div id = "child">
-    <h1>I am h1 tag</h1>
-    <h2>I am h2 tag </h2>
-  </div>
-</div> 
-how to create slibling ?
-1. we have to pass one child and
-2. convert into array pass to second child and create array of childern
-
-ReactElement(object) => HTML (browser understands)
-*/
-
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I am h1 tag"),
