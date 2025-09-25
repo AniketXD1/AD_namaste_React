577d81564a22bbb8d72c64b92a163d59cd8a450d
AniketXD1 <tsmaniket@gmail.com>
2025-09-25 15:57:56 +0530

    episode2

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", {}, "I am h1 tag"),
+//     React.createElement("h2", {}, "I am h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I am h1 tag"),
+//     React.createElement("h2", {}, "I am h2 tag"),
+//   ]),
+// ]);
+// // console.log(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+//Error: Each child in a list should have a unique "key" prop.
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { key: "h1-1" }, "Hii i am leraning react"),
+    React.createElement("h2", { key: "h2-1" }, "I am h2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "h1-2" }, "I am h1 tag"),
+    React.createElement("h2", { key: "h2-2" }, "I am h2 tag"),
   ]),
 ]);
-console.log(parent);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Namaste React 🚀
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot module Replacement
+- File Watching Algorithm - Written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- consistent Hashing - read more about it interting topic
+- Code Spliting
+- Differntial Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
